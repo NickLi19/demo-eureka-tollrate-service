c3f5fa5ea83a9268bb8634dc8617997b0c2421fb
Jianmin <nicklee19950330@gmail.com>
2020-08-24 01:51:34 -0500

    eurake tollrate service

---
+++ src/main/java/eureka/demo/CustomHealthCheck.java
@@ -0,0 +1,19 @@
+package eureka.demo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthCheck implements HealthIndicator {
+	int errorCode = 0;
+	public Health health() {
+		System.out.println("Health check performed! Error Code is: " + errorCode);
+		if (errorCode > 3 && errorCode < 9) {
+			errorCode++;
+			return Health.down().withDetail("Custom Error Code", errorCode).build();
+		}
+		errorCode++;
+		return Health.up().build();
+	}
+}
